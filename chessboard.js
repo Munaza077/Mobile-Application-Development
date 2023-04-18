3d0b5a964a4c1fb4223126bd157ecb4b0f24111f
Munaza077 <113824405+Munaza077@users.noreply.github.com>
2023-04-18 14:18:48 +0500

    Update chessboard.js

--- chessboard.js
+++ chessboard.js
@@ -1,86 +1,90 @@
-
-
-/*Chess game*/
-
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Button } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
 const ChessBoard = () => {
-  const [boardState, setBoardState] = useState([
-    ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR'],
-    ['BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP', 'BP'],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP', 'WP'],
-    ['WR', 'WN', 'WB', 'WQ', 'WK', 'WB', 'WN', 'WR'],
-  ]);
-
-  const [selectedPiece, setSelectedPiece] = useState(null);
-
-  const handleSquarePress = (row, col) => {
-    if (selectedPiece) {
-      // Move selected piece to new square
-      const newBoardState = [...boardState];
-      newBoardState[row][col] = selectedPiece;
-      newBoardState[selectedPiece.row][selectedPiece.col] = '';
-      setBoardState(newBoardState);
-
-      setSelectedPiece(null);
-    } else if (boardState[row][col]) {
-      // Select piece on square
-      setSelectedPiece({ piece: boardState[row][col], row, col });
-    }
-  };
-
-  const renderSquare = (row, col) => {
-    const backgroundColor = (row + col) % 2 === 0 ? '#f0d9b5' : '#b58863';
+  const renderSquare = (x, y) => {
+    const isEven = (x + y) % 2 === 0;
 
     return (
-      <Button
-        mode="contained"
-        style={[styles.square, { backgroundColor }]}
-        onPress={() => handleSquarePress(row, col)}
-      >
-        {boardState[row][col]}
-      </Button>
+      <View
+        key={`${x}-${y}`}
+        style={[
+          styles.square,
+          { backgroundColor: isEven ? '#d18b47' : '#ffce9e' },
+        ]}
+      />
+    );
+  };
+
+  const renderRankLabel = (y) => {
+    return (
+      <View key={`rank-${y}`} style={styles.label}>
+        <Text style={styles.text}>{8 - y}</Text>
+      </View>
+    );
+  };
+
+  const renderFileLabel = (x) => {
+    const labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    return (
+      <View key={`file-${x}`} style={styles.label}>
+        <Text style={styles.text}>{labels[x]}</Text>
+      </View>
     );
   };
 
   return (
     <View style={styles.container}>
-      {[...Array(8)].map((_, row) => (
-        <View key={row} style={styles.row}>
-          {[...Array(8)].map((_, col) => (
-            <View key={col}>{renderSquare(row, col)}</View>
-          ))}
-        </View>
-      ))}
+      <View style={styles.labelsContainer}>
+        {Array.from(Array(8), (_, y) => renderRankLabel(y))}
+      </View>
+      <View style={styles.boardContainer}>
+        {Array.from(Array(8), (_, y) =>
+          Array.from(Array(8), (_, x) => renderSquare(x, y))
+        )}
+      </View>
+      <View style={[styles.labelsContainer, styles.filesContainer]}>
+        {Array.from(Array(8), (_, x) => renderFileLabel(x))}
+      </View>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 8,
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-    
-
-  },
-  row: {
     flexDirection: 'row',
   },
-  square: {
-    width: 60,
-    height: 60,
+  labelsContainer: {
+    width: 20,
+    alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'black',
-    
+  },
+  filesContainer: {
+    height: 20,
+    flexDirection: 'row',
+  },
+  boardContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: 8 * 40,
+    height: 8 * 40,
+  },
+  square: {
+    width: 40,
+    height: 40,
+  },
+  label: {
+    width: 20,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: 'white',
   },
 });
 
-export default ChessBoard; 
+export default ChessBoard;
